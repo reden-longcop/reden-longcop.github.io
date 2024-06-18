41ea611f2f36dac93c23b8195a818ea96ff44115
unknown <redenlongcop5@gmail.com>
2024-06-18 10:30:04 +0800

    forda commit

--- src/components/profile.js
+++ src/components/profile.js
@@ -32,15 +32,15 @@ function Profile() {
                 }}
             >
                 <h1 className="md:text-8xl text-5xl">REDEN LONGCOP</h1>
-                <h3>QA Engineer / Frontend Developer</h3>
-                <div className="icons space-x-2">
+                <h3>QA Engineer | Frontend Developer</h3>
+                <div className="icons md:space-x-6 sm:space-x-2">
                     <a href={linkedin_link} className="icon-link"><FontAwesomeIcon className="linkedin icon" icon={faLinkedin} /></a>
                     <a href={github_link} className="icon-link"><FontAwesomeIcon className="github icon" icon={faGithub} /></a>
                     <a href={emailto_link} className="icon-link"><FontAwesomeIcon className="email icon" icon={faEnvelope} /></a>
                 </div>
             </div>
             <div 
-                className="profilePic md:block sm:hidden"
+                className="profilePic md:block"
                 ref={profilePicRef}
                 style={{
                     opacity: isProfilePicVisible ? 1 : 0,
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,12 +1,19 @@
 import React, { useRef } from 'react';
-import jira from '../images/icons/jira.png';
-import git from '../images/icons/git.png';
-import postman from '../images/icons/postman.png';
-import jmeter from '../images/icons/jmeter.png';
-import lambda from '../images/icons/lambda.png';
 import useIsVisible from './useIsVisible';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardList, faExclamationTriangle, faNetworkWired, faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGitAlt, faJira } from '@fortawesome/free-brands-svg-icons';
+// import { faDownload } from "@fortawesome/free-solid-svg-icons/faDownload";
 
 const Skills = () => {
+    const toolSets = [
+        { id: 1, icon: faJira, label: 'Proficient in using bug tracking tool like JIRA' },
+        { id: 1, icon: faGitAlt, label: 'Proficient in using GIT commands' },
+        { id: 1, icon: faNetworkWired, label: 'Familiarity in using API testing using POSTMAN' },
+        { id: 1, icon: faTachometerAlt, label: 'Familiarity in performance testing tools like JMETER and LOCUST' },
+        { id: 1, icon: faClipboardList, label: 'Proficient in using bug tracking tool like JIRA' },
+        { id: 1, icon: faExclamationTriangle, label: 'Proficient in using bug tracking tool like JIRA' },
+    ];
     const skillSets = [
         {
             id: 1,
@@ -34,37 +41,42 @@ const Skills = () => {
     const isVisible = useIsVisible(skillsRef, { threshold: 0.3 });
 
     return (
-        <div id="skills" className="flex flex-col items-center" ref={skillsRef}>
-            <h2 className={`md:text-4xl text-xl m-4 ${isVisible ? 'visible' : 'invisible'}`}
-                style={{
-                    opacity: isVisible ? 1 : 0,
-                    transition: 'opacity 0.5s ease-in-out',
-                    transitionDelay: isVisible ? '0.5s' : '0s'
-                }}
-            >SKILLS</h2>
-            <div className="sets flex flex-wrap justify-center">
-                {skillSets.map((set) => (
-                    <SkillSet key={set.id} skills={set.skills} isVisible={isVisible} />
-                ))}
+        <div id="skills" className="sm:flex sm:flex-center sm:items-center" ref={skillsRef}>
+            <div className='skills-con-1'>
+                <h2 className={`md:text-4xl text-xl m-4 ${isVisible ? 'visible' : 'invisible'}`}
+                    style={{
+                        opacity: isVisible ? 1 : 0,
+                        transition: 'opacity 0.5s ease-in-out',
+                        transitionDelay: isVisible ? '0.5s' : '0s'
+                    }}
+                >SKILLS</h2>
+                <div className="sets flex flex-wrap justify-center">
+                    {skillSets.map((set) => (
+                        <SkillSet key={set.id} skills={set.skills} isVisible={isVisible} />
+                    ))}
+                </div>
             </div>
-            <h2 className={`md:text-4xl text-xl m-4 ${isVisible ? 'visible' : 'invisible'}`}
-                style={{
-                    opacity: isVisible ? 1 : 0,
-                    transition: 'opacity 0.5s ease-in-out',
-                    transitionDelay: isVisible ? '0.5s' : '0s'
-                }}
-            >TOOLS</h2>
-            <div className={`tools-set grid grid-cols-5 gap-4 ${isVisible ? 'fade-in' : 'fade-out'}}`}
-                style={{
-                    opacity: isVisible ? 1 : 0,
-                    transition: 'opacity 0.5s ease-in-out',
-                }}
-            >
-                <img src={jira} alt="jira" />
-                <img src={git} alt="git" />
-                <img src={postman} alt="postman" />
-                <img className="col-span-2" src={jmeter} alt="jmeter" />
-                <img className="col-span-5" src={lambda} alt="lambda" />
+            <div className='skills-con-2'>
+                {/* <h2 className={`md:text-4xl text-xl m-4 ${isVisible ? 'visible' : 'invisible'}`}
+                    style={{
+                        opacity: isVisible ? 1 : 0,
+                        transition: 'opacity 0.5s ease-in-out',
+                        transitionDelay: isVisible ? '0.5s' : '0s'
+                    }}
+                >TOOLS</h2> */}
+                <div className="tools-set-con">
+                    {toolSets.map((tool) => (
+                        <div key={tool.id} className={`tools-set md:m-5 ${isVisible ? 'fade-in' : 'fade-out'}`}
+                            style={{
+                                opacity: isVisible ? 1 : 0,
+                                transition: 'opacity 0.5s ease-in-out',
+                            }}
+                        >
+                            <label>{tool.label}</label>
+                            <FontAwesomeIcon className="tool-icon" icon={tool.icon} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
